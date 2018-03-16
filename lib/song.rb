757a53a3f61639f55bbfe8fd9a9ed6f6a376cf4b
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 09:44:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,11 @@ class Song
   end
   
   def self.new_from_filename(file)
-    self.new(file.split(" - ")[1], Artist.find_or_create_by_name(file.split(" - ")[0]), Genre.find_or_create_by_name(file.split(" - ")[2].chomp(".mp3")))
+    self.new(
+      file.split(" - ")[1], 
+      Artist.find_or_create_by_name(file.split(" - ")[0]), 
+      Genre.find_or_create_by_name(file.split(" - ")[2].chomp(".mp3"))
+      )
   end
 
 #  def self.new_from_filename(file)
