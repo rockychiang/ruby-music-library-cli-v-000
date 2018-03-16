f5cdb58f0250cc7dcbebf09dc09943dde4e98072
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 09:43:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,9 @@ class Song
 #  end
   
   def self.create_from_filename(file)
-    self.new_from_filename(file)
-    
+    self.new_from_filename(file).tap do |song|
+      song.save
+    end
   end
   
 end
