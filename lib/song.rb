59126e7d42167897cf45dc03f65d712037d261ea
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 09:29:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.new_from_filename(file)
     self.find_or_create_by_name(file.split(" - ")[1]).tap do |song|
       song.artist = Artist.find_or_create_by_name(file.split(" - ")[0])
-      song.genre = Genre.find_or_create_by_name(file.split(" - ")[2])
+      song.genre = Genre.find_or_create_by_name(file.split(" - ")[2].chomp(".mp3"))
     end
   end
   
