46132b776b85992704919510575f339a18e914d5
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 08:23:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,9 @@ class Song
     artist.add_song(self)
   end
   
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
   end
   
 end
