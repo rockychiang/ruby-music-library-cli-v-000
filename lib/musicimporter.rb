d4de2ddbe47fc71db34f9555a59de3009b772e1e
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 09:54:12 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -10,7 +10,7 @@ class MusicImporter
   end
   
   def import
-    self.files.each{|file| Song.create_fromm_filename(file)}
+    self.files.each{|file| Song.create_from_filename(file)}
   end
 
 end
