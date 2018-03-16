3c8eb4890ec12b5cd1fb0e1729968bf26f24500f
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 08:48:15 +0000

    automatically backed up by learn

--- lib/concern.rb
+++ lib/concern.rb
@@ -27,11 +27,7 @@ module Concerns
     end
     
     def find_or_create_by_name(name)
-      if self.find_by_name(name)
-        self.find_by_name(name)
-      else
-        self.create(name)
-      end
+      self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
     end
   end
 end
